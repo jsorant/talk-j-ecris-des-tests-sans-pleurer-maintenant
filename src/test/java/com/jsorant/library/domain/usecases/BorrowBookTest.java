a60cf54870d2fed62c9ad8a8561632c0f08298f7
Jérémy Sorant <jsorant@gmail.com>
2025-03-31 11:01:09 +0200

    review test code

--- src/test/java/com/jsorant/library/domain/usecases/BorrowBookTest.java
+++ src/test/java/com/jsorant/library/domain/usecases/BorrowBookTest.java
@@ -1,65 +1,116 @@
 package com.jsorant.library.domain.usecases;
 
-import com.jsorant.UnitTest;
+import com.jsorant.library.domain.Book;
+import com.jsorant.library.domain.BookType;
+import com.jsorant.library.domain.Borrows;
 import com.jsorant.library.domain.events.BookBorrowed;
 import com.jsorant.library.domain.exceptions.BookAlreadyBorrowedException;
 import com.jsorant.library.domain.exceptions.BookNotOwnedByTheLibraryException;
 import com.jsorant.library.domain.exceptions.BorrowerHasAlreadyFourBooksBorrowedException;
-import org.junit.jupiter.api.DisplayName;
+import com.jsorant.library.secondary.InMemoryBookRepository;
+import com.jsorant.library.secondary.InMemoryBorrowRepository;
 import org.junit.jupiter.api.Test;
 
-import static com.jsorant.library.LibraryAssertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.time.Instant;
 
-@UnitTest
-@DisplayName("Borrow a book")
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// Book must be owned by the library
+// Book must not be already borrowed
+// Borrower must not be already borrowing four books
+// DomainEvent must be returned
+
 public class BorrowBookTest {
 
-    BorrowBookContext context = new BorrowBookContext();
-
     @Test
-    void shouldReturnBookBorrowedEventWhenBookIsBorrowed() {
-        BorrowBook borrowBook = context.buildBorrowBook();
+    void test1() {
+        InMemoryBookRepository bookRepository = new InMemoryBookRepository();
+        InMemoryBorrowRepository borrowsRepository = new InMemoryBorrowRepository();
+        bookRepository.save(new Book("3214515512", "The Lord of the Rings", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("1234567890", "The Hobbit", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("4083U14844", "Harry Potter and the Philosopher's Stone", "JK Rowling", BookType.NOVEL));
+        try { // already borrowed
+            Borrows bobBorrows = new Borrows("bob.doe@domain.fr").borrow("1234567890", Instant.parse("2025-04-13T10:00:00Z"));
+            borrowsRepository.save(bobBorrows);
+            new BorrowBook(bookRepository, borrowsRepository)
+                    .as("alice.doe@domain.fr")
+                    .bookId("1234567890")
+                    .date(Instant.parse("2025-04-14T10:00:00Z"))
+                    .act();
 
-        BookBorrowed bookBorrowed = borrowBook.act();
-
-        assertThat(bookBorrowed)
-                .refersToBook(context.bookToBorrowId())
-                .refersToBorrower(context.borrowerEmail())
-                .wasDoneOn(context.borrowDate());
+            fail("Should have thrown an exception");
+        } catch (RuntimeException e) {
+            assertEquals(true, e instanceof BookAlreadyBorrowedException);
+            assertTrue(e.getMessage().contains("1234567890"));
+        }
     }
 
     @Test
-    void shouldThrowWhenBookIsNotOwnedByTheLibrary() {
-        BorrowBook borrowBook = context
-                .butBorrowingABookThatIsNotOwnedByTheLibrary()
-                .buildBorrowBook();
-
-        assertThatThrownBy(borrowBook::act)
-                .isInstanceOf(BookNotOwnedByTheLibraryException.class)
-                .hasMessageContaining(context.idOfTheBookToBorrowThatIsNotOwnedByTheLibrary());
+    void test2() {
+        InMemoryBookRepository bookRepository = new InMemoryBookRepository();
+        InMemoryBorrowRepository borrowsRepository = new InMemoryBorrowRepository();
+        bookRepository.save(new Book("3214515512", "The Lord of the Rings", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("1234567890", "The Hobbit", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("4083U14844", "Harry Potter and the Philosopher's Stone", "JK Rowling", BookType.NOVEL));
+        BookBorrowed result = new BorrowBook(bookRepository, borrowsRepository)
+                .as("alice.doe@domain.fr")
+                .bookId("3214515512")
+                .date(Instant.parse("2025-04-14T10:00:00Z"))
+                .act();
+        assertEquals(new BookBorrowed("alice.doe@domain.fr", "3214515512", Instant.parse("2025-04-14T10:00:00Z")), result);
     }
 
     @Test
-    void shouldThrowWhenBookIsAlreadyBorrowed() {
-        BorrowBook borrowBook = context
-                .butWithBookToBorrowAlreadyBorrowed()
-                .buildBorrowBook();
+    void test3() {
+        InMemoryBookRepository bookRepository = new InMemoryBookRepository();
+        InMemoryBorrowRepository borrowsRepository = new InMemoryBorrowRepository();
 
-        assertThatThrownBy(borrowBook::act)
-                .isInstanceOf(BookAlreadyBorrowedException.class)
-                .hasMessageContaining(context.bookToBorrowId());
+        try {
+            new BorrowBook(bookRepository, borrowsRepository)
+                    .as("alice.doe@domain.fr")
+                    .bookId("1234567891")
+                    .date(Instant.parse("2025-04-14T10:00:00Z"))
+                    .act();
+
+            fail("Should have thrown an exception");
+        } catch (RuntimeException e) {
+            assertEquals(true, e instanceof BookNotOwnedByTheLibraryException);
+            assertTrue(e.getMessage().contains("1234567891"));
+        }
     }
 
     @Test
-    void shouldThrowWhenBorrowerHasAlreadyFourBooksBorrowed() {
-        BorrowBook borrowBook = context
-                .butWithAlreadyFourBooksBorrowed()
-                .buildBorrowBook();
+    void test4() {
+        InMemoryBookRepository bookRepository = new InMemoryBookRepository();
+        bookRepository.save(new Book("4083U14844", "Harry Potter and the Philosopher's Stone", "JK Rowling", BookType.NOVEL));
+        bookRepository.save(new Book("3214515512", "The Lord of the Rings", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("5341343136", "The Two Towers", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("6453424356", "The Return of the King", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("2534646466", "The Fellowship of the Ring", "JRR Tolkien", BookType.NOVEL));
+        bookRepository.save(new Book("1234567890", "The Hobbit", "JRR Tolkien", BookType.NOVEL));
 
-        assertThatThrownBy(borrowBook::act)
-                .isInstanceOf(BorrowerHasAlreadyFourBooksBorrowedException.class)
-                .hasMessageContaining(context.bookToBorrowId())
-                .hasMessageContaining(context.borrowerEmail());
+        InMemoryBorrowRepository borrowsRepository = new InMemoryBorrowRepository();
+        Borrows aliceBorrows = new Borrows("alice.doe@domain.fr")
+                .borrow("2534646466", Instant.parse("2025-04-13T10:00:00Z"))
+                .borrow("6453424356", Instant.parse("2025-04-14T10:00:00Z"))
+                .borrow("3214515512", Instant.parse("2025-04-15T10:00:00Z"))
+                .borrow("5341343136", Instant.parse("2025-04-16T10:00:00Z"));
+        borrowsRepository.save(aliceBorrows);
+
+        try {
+            new BorrowBook(bookRepository, borrowsRepository)
+                    .as("alice.doe@domain.fr")
+                    .bookId("1234567890")
+                    .date(Instant.parse("2025-04-14T10:00:00Z"))
+                    .act();
+
+            fail("Should have thrown an exception");
+        } catch (RuntimeException e) {
+            assertEquals(true, e instanceof BorrowerHasAlreadyFourBooksBorrowedException);
+            assertTrue(e.getMessage().contains("1234567890"));
+            assertTrue(e.getMessage().contains("alice.doe@domain.fr"));
+        }
     }
 }
---
+++ src/test/java/com/jsorant/library/domain/usecases/ReworkedBorrowBookTest.java
@@ -0,0 +1,65 @@
+package com.jsorant.library.domain.usecases;
+
+import com.jsorant.UnitTest;
+import com.jsorant.library.domain.events.BookBorrowed;
+import com.jsorant.library.domain.exceptions.BookAlreadyBorrowedException;
+import com.jsorant.library.domain.exceptions.BookNotOwnedByTheLibraryException;
+import com.jsorant.library.domain.exceptions.BorrowerHasAlreadyFourBooksBorrowedException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.jsorant.library.LibraryAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@UnitTest
+@DisplayName("Borrow a book")
+public class ReworkedBorrowBookTest {
+
+    BorrowBookContext context = new BorrowBookContext();
+
+    @Test
+    void shouldReturnBookBorrowedEventWhenBookIsBorrowed() {
+        BorrowBook borrowBook = context.buildBorrowBook();
+
+        BookBorrowed bookBorrowed = borrowBook.act();
+
+        assertThat(bookBorrowed)
+                .refersToBook(context.bookToBorrowId())
+                .refersToBorrower(context.borrowerEmail())
+                .wasDoneOn(context.borrowDate());
+    }
+
+    @Test
+    void shouldThrowWhenBookIsNotOwnedByTheLibrary() {
+        BorrowBook borrowBook = context
+                .butBorrowingABookThatIsNotOwnedByTheLibrary()
+                .buildBorrowBook();
+
+        assertThatThrownBy(borrowBook::act)
+                .isInstanceOf(BookNotOwnedByTheLibraryException.class)
+                .hasMessageContaining(context.idOfTheBookToBorrowThatIsNotOwnedByTheLibrary());
+    }
+
+    @Test
+    void shouldThrowWhenBookIsAlreadyBorrowed() {
+        BorrowBook borrowBook = context
+                .butWithBookToBorrowAlreadyBorrowed()
+                .buildBorrowBook();
+
+        assertThatThrownBy(borrowBook::act)
+                .isInstanceOf(BookAlreadyBorrowedException.class)
+                .hasMessageContaining(context.bookToBorrowId());
+    }
+
+    @Test
+    void shouldThrowWhenBorrowerHasAlreadyFourBooksBorrowed() {
+        BorrowBook borrowBook = context
+                .butWithAlreadyFourBooksBorrowed()
+                .buildBorrowBook();
+
+        assertThatThrownBy(borrowBook::act)
+                .isInstanceOf(BorrowerHasAlreadyFourBooksBorrowedException.class)
+                .hasMessageContaining(context.bookToBorrowId())
+                .hasMessageContaining(context.borrowerEmail());
+    }
+}
